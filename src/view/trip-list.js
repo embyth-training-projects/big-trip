e78e1efe3d7bda983f0f63c8e44c3e2668bf0558
Rostyslav Miniukov <thresh1337@gmail.com>
2020-11-10 19:33:45 +0200

    Обучаем шаблон списка точек маршрута принимать данные. WIP

--- src/view/trip-list.js
+++ src/view/trip-list.js
@@ -1,3 +1,33 @@
-export const createTripListTemplate = () => {
-  return `<ul class="trip-days"></ul>`;
+import {createTripItemTemplate} from './trip-item';
+
+const formatDate = (date) => {
+  return date.toLocaleString(`en-En`, {day: `2-digit`, month: `2-digit`, year: `2-digit`});
+};
+
+const getTripDays = (events) => {
+  const dates = events.map((day) => formatDate(day.dateRange[0]));
+  return [...new Set(dates)];
+};
+
+const filterEventsByDay = (events, day) => {
+  return events.filter((event) => formatDate(event.dateRange[0]) === day);
+};
+
+export const createTripListTemplate = (events) => {
+  const days = getTripDays(events);
+
+  if (days.length) {
+    return (
+      `<ul class="trip-days">
+        ${days
+          .map((day, index) => {
+            const filteredEventsByDay = filterEventsByDay(events, day);
+            return createTripItemTemplate(filteredEventsByDay, day, index + 1);
+          })
+          .join(``)}
+      </ul>`
+    );
+  } else {
+    return `<p class="trip-events__msg">Click New Event to create your first point</p>`;
+  }
 };
