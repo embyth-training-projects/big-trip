bb28fc8bd36331b179e2cac6051e74f099c8fbc6
Rostyslav Miniukov <thresh1337@gmail.com>
2020-11-10 22:29:52 +0200

    Используем данные для оставшихся шаблонов и сортируем точки согласно дате

--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@ import {createSiteMenuTemplate} from './view/site-menu';
 import {createTripInfoTemplate} from './view/trip-info';
 import {createTripSortTemplate} from './view/sort';
 import {createTripListTemplate} from './view/trip-list';
-import {createTripItemTemplate} from './view/trip-item';
 import {createTripFormTemplate} from './view/trip-form';
 import {generateTrip} from './mock/trip';
 
-const TRIP_EVENTS = 20;
+const TRIP_EVENTS_COUNT = 20;
 
-const events = new Array(TRIP_EVENTS).fill().map(generateTrip);
+const events = new Array(TRIP_EVENTS_COUNT)
+  .fill()
+  .map(generateTrip)
+  .sort((a, b) => a.dateRange[0].getTime() - b.dateRange[0].getTime());
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -16,16 +18,11 @@ const render = (container, template, place) => {
 
 const siteHeaderElement = document.querySelector(`.page-header`);
 const tripMainElement = siteHeaderElement.querySelector(`.trip-main`);
-render(tripMainElement, createTripInfoTemplate(), `beforeend`);
+render(tripMainElement, createTripInfoTemplate(events), `beforeend`);
 render(tripMainElement, createSiteMenuTemplate(), `beforeend`);
 
 const siteMainElement = document.querySelector(`.page-main`);
 const tripEventsElement = siteMainElement.querySelector(`.trip-events`);
 render(tripEventsElement, createTripSortTemplate(), `beforeend`);
-render(tripEventsElement, createTripFormTemplate(), `beforeend`);
-render(tripEventsElement, createTripListTemplate(), `beforeend`);
-
-const tripListElement = siteMainElement.querySelector(`.trip-days`);
-for (let i = 0; i < TRIP_EVENTS; i++) {
-  render(tripListElement, createTripItemTemplate(events[i]), `beforeend`);
-}
+render(tripEventsElement, createTripFormTemplate(events[0]), `beforeend`);
+render(tripEventsElement, createTripListTemplate(events.slice(1)), `beforeend`);
