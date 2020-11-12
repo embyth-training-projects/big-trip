620b33833d1b2806f5db9d2dbfd4140a360ed8bc
Rostyslav Miniukov <thresh1337@gmail.com>
2020-11-12 17:45:51 +0200

    Доработка в шаблоне формы точки маршрута

--- src/view/trip-form.js
+++ src/view/trip-form.js
@@ -176,13 +176,14 @@ const createTripFormTemplate = (event) => {
 
         ${offersTemplate}
 
-        <section class="event__section  event__section--destination">
+      ${(description || photos.length)
+      ? `<section class="event__section  event__section--destination">
           <h3 class="event__section-title  event__section-title--destination">Destination</h3>
           <p class="event__destination-description">${description}</p>
-
           ${photosTemplate}
+        </section>`
+      : ``}
 
-        </section>
       </section>
     </form>`
   );
