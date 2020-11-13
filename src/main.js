679fdee2a3e9f10cc82cab6c92ae874233678505
Rostyslav Miniukov <thresh1337@gmail.com>
2020-11-13 16:52:54 +0200

    Используем презентер вместо набора функций

--- src/main.js
+++ src/main.js
@@ -2,16 +2,10 @@ import MenuView from './view/menu';
 import FilterView from './view/filter';
 import NewEventButtonView from './view/new-event-button';
 import TripInfoView from './view/trip-info';
-import SortView from './view/sort';
-import TripListView from './view/trip-list';
-import TripDayView from './view/trip-item';
-import TripEventView from './view/trip-event';
-import TripFormView from './view/trip-form';
-import NoEventView from './view/no-event';
+import TimelinePresenter from './presenter/timeline';
 import {generateTrip} from './mock/trip';
-import {getTripDays, filterEventsByDay} from './utils/trip';
-import {render, replace} from './utils/render';
-import {TRIP_EVENTS_COUNT, RenderPosition, KeyCode} from './const';
+import {render} from './utils/render';
+import {TRIP_EVENTS_COUNT, RenderPosition} from './const';
 
 const events = new Array(TRIP_EVENTS_COUNT)
   .fill()
@@ -23,69 +17,11 @@ const menuContainer = tripMainElement.querySelector(`h2:nth-child(1)`);
 const filterContainer = tripMainElement.querySelector(`h2:nth-child(2)`);
 const tripEventsElement = document.querySelector(`.trip-events`);
 
+const timelinePresenter = new TimelinePresenter(tripEventsElement);
+
 render(tripMainElement, new TripInfoView(events), RenderPosition.AFTERBEGIN);
 render(menuContainer, new MenuView(), RenderPosition.AFTEREND);
 render(filterContainer, new FilterView(), RenderPosition.AFTEREND);
 render(tripMainElement, new NewEventButtonView(), RenderPosition.BEFOREEND);
 
-const addEventsActions = (eventComponent, eventEditComonent) => {
-  const replacePointToForm = () => {
-    replace(eventEditComonent, eventComponent);
-  };
-
-  const replaceFormToPoint = () => {
-    replace(eventComponent, eventEditComonent);
-  };
-
-  const onEscKeyDown = (evt) => {
-    if (evt.keyCode === KeyCode.ESC || evt.key === `Esc` || evt.code === `Escape`) {
-      evt.preventDefault();
-      replaceFormToPoint();
-      document.removeEventListener(`keydown`, onEscKeyDown);
-    }
-  };
-
-  eventComponent.setEditClickHandler(() => {
-    replacePointToForm();
-    document.addEventListener(`keydown`, onEscKeyDown);
-  });
-
-  eventEditComonent.setFormSubmitHandler(() => {
-    replaceFormToPoint();
-    document.removeEventListener(`keydown`, onEscKeyDown);
-  });
-
-  eventEditComonent.setFormResetHandler(() => {
-    replaceFormToPoint();
-    document.removeEventListener(`keydown`, onEscKeyDown);
-  });
-};
-
-const createEventsList = () => {
-  const eventsListComponent = new TripListView(events);
-
-  getTripDays(events).forEach((day, index) => {
-    const tripDayComponent = new TripDayView(day, index + 1);
-    render(eventsListComponent, tripDayComponent, RenderPosition.BEFOREEND);
-    const filteredEventsByDay = filterEventsByDay(events, day);
-
-    filteredEventsByDay.forEach((event) => {
-      const eventComponent = new TripEventView(event);
-      const eventEditComonent = new TripFormView(event);
-
-      const eventDayContainer = tripDayComponent.getElement().querySelector(`.trip-events__list[data-day="${day}"]`);
-      render(eventDayContainer, eventComponent, RenderPosition.BEFOREEND);
-      addEventsActions(eventComponent, eventEditComonent);
-    });
-  });
-
-  return eventsListComponent;
-};
-
-if (events.length) {
-  render(tripEventsElement, new SortView(), RenderPosition.BEFOREEND);
-  render(tripEventsElement, createEventsList(), RenderPosition.BEFOREEND);
-} else {
-  render(tripEventsElement, new NoEventView(), RenderPosition.BEFOREEND);
-}
-
+timelinePresenter.init(events);
