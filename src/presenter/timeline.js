00b13e9f2fbdeeaa438273d135a7c814c5d48a4e
Rostyslav Miniukov <thresh1337@gmail.com>
2020-11-13 16:21:18 +0200

    Описываем логику презентера ленты. WIP

--- src/presenter/timeline.js
+++ src/presenter/timeline.js
@@ -4,11 +4,14 @@ import TripDayView from './view/trip-item';
 import TripEventView from './view/trip-event';
 import TripFormView from './view/trip-form';
 import NoEventView from './view/no-event';
+import {render} from '../utils/render';
+import {RenderPosition} from '../const';
 
 export default class Timeline {
   constructor(timelineContainer) {
     this._timelineContainer = timelineContainer;
 
+    this._timelineEvents = null;
     this._timelineComponent = null;
 
     this._sortComponent = new SortView();
@@ -17,6 +20,11 @@ export default class Timeline {
 
   init(events) {
     this._timelineEvents = events.slice();
+
+    this._timelineComponent = new TripListView(this._timelineEvents);
+    render(this._timelineContainer, this._timelineComponent, RenderPosition.BEFOREEND);
+
+    this._renderTimeline();
   }
 
   _renderSort() {
@@ -36,6 +44,12 @@ export default class Timeline {
   }
 
   _renderTimeline() {
+    if (!this._timelineEvents.length) {
+      this._renderNoEvents();
+      return;
+    }
 
+    this._renderSort();
+    this._renderEvents();
   }
 }
