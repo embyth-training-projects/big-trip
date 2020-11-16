ad6f0159201a37cb9c7e7dfa299bee90d1241c9a
Rostyslav Miniukov <thresh1337@gmail.com>
2020-11-16 15:10:47 +0200

    Обучаем компонент обновлять данные

--- src/view/trip-form.js
+++ src/view/trip-form.js
@@ -218,6 +218,20 @@ export default class TripForm extends AbstractView {
     return createTripFormTemplate(this._event);
   }
 
+  updateData(update) {
+    if (!update) {
+      return;
+    }
+
+    this._data = Object.assign(
+        {},
+        this._data,
+        update
+    );
+
+    this.updateElement();
+  }
+
   updateElement() {
     let prevElement = this.getElement();
     const parent = prevElement.parentElement;
